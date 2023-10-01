0b8dedc3ecfb87e38f63a7aca9c4c0b8c629c3ff
eonianmonk <no.mourner@gmail.com>
2023-10-01 23:39:50 +0300

    moved cli cfg

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	logger "log"
 
+	"github.com/alecthomas/kingpin"
 	"github.com/eonianmonk/go-rate-limit/backend/config"
 	"github.com/eonianmonk/go-rate-limit/backend/http"
 	"github.com/kkyr/fig"
@@ -17,11 +18,20 @@ func Run(args []string) {
 		}
 	}()
 
+	app := kingpin.New("ratel", "api with limited rate")
+
+	runCmd := app.Command("run", "run svc")
+
+	migrateCmd := app.Command("migrate", "migrate cmd")
+	migrateUpCmd := migrateCmd.Command("up", "migrate up cmd")
+	// flags
+	configName := app.Flag("cfg", "config file name").Default("config.yaml").String()
+	rateLimit := app.Flag("max-rate", "rate limit").Default("50").Int16()
+
 	cmd, err := app.Parse(args[:1])
 	if err != nil {
 		log.Fatal(errors.Wrap(err, "Failed to parse cli command"))
 	}
-
 	cfg := config.New(fig.File(*configName), rateLimit)
 
 	switch cmd {
